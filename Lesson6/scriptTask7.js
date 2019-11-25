f0e6d03784ec32558e1360d7dba67e98c922cca4
OlenaPauk <50915760+OlenaPauk@users.noreply.github.com>
2019-11-25 12:36:06 +0200

    Update scriptTask7.js

--- Lesson6/scriptTask7.js
+++ Lesson6/scriptTask7.js
@@ -1,21 +1,4 @@
 // 7.1
-// let mainT = document.createElement("main");
-// mainT.classList.add("mainClass");
-// mainT.classList.add("check");
-// mainT.classList.add("item");
-
-// let divT = document.createElement("div");
-// divT.id = "myDiv";
-
-// let pT = document.createElement("p");
-// pT.innerHTML="First paragraph";
-
-// divT.prepend(pT);
-// mainT.prepend(divT);
-// document.body.append(mainT);
-// console.log(document.body.outerHTML);
-
-// 7.2
 let mainT = document.createElement("main");
 mainT.classList.add("mainClass");
 mainT.classList.add("check");
@@ -24,8 +7,26 @@ mainT.classList.add("item");
 let divT = document.createElement("div");
 divT.id = "myDiv";
 
-divT.insertAdjacentHTML("afterbegin","<p>First paragraph</p>");
-mainT.appendChild(divT);
+let pT = document.createElement("p");
+pT.innerHTML="First paragraph";
+
+divT.prepend(pT);
+mainT.prepend(divT);
 document.body.append(mainT);
 console.log(document.body.outerHTML);
+
+// 7.2
+// let mainT = document.createElement("main");
+// mainT.classList.add("mainClass");
+// mainT.classList.add("check");
+// mainT.classList.add("item");
+
+// let divT = document.createElement("div");
+// divT.id = "myDiv";
+
+// divT.insertAdjacentHTML("afterbegin","<p>First paragraph</p>");
+// mainT.appendChild(divT);
+// document.body.append(mainT);
+// console.log(document.body.outerHTML);
+
 
