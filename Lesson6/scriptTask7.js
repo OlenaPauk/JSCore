74cbf56dd524f81fc65b478646182e47aee9bfe3
OlenaPauk <50915760+OlenaPauk@users.noreply.github.com>
2019-11-25 12:35:12 +0200

    Add files via upload

---
+++ Lesson6/scriptTask7.js
@@ -0,0 +1,31 @@
+// 7.1
+// let mainT = document.createElement("main");
+// mainT.classList.add("mainClass");
+// mainT.classList.add("check");
+// mainT.classList.add("item");
+
+// let divT = document.createElement("div");
+// divT.id = "myDiv";
+
+// let pT = document.createElement("p");
+// pT.innerHTML="First paragraph";
+
+// divT.prepend(pT);
+// mainT.prepend(divT);
+// document.body.append(mainT);
+// console.log(document.body.outerHTML);
+
+// 7.2
+let mainT = document.createElement("main");
+mainT.classList.add("mainClass");
+mainT.classList.add("check");
+mainT.classList.add("item");
+
+let divT = document.createElement("div");
+divT.id = "myDiv";
+
+divT.insertAdjacentHTML("afterbegin","<p>First paragraph</p>");
+mainT.appendChild(divT);
+document.body.append(mainT);
+console.log(document.body.outerHTML);
+
